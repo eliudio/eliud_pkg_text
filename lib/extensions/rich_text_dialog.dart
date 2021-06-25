037c80cdb95e44dc5b7a4b4ceda771673b01ac3f
Eliud <58745656+eliudio@users.noreply.github.com>
2021-06-25 12:58:00 +0100

    Bufixes

--- lib/extensions/rich_text_dialog.dart
+++ lib/extensions/rich_text_dialog.dart
@@ -1,4 +1,6 @@
 import 'package:eliud_core/default_style/frontend/impl/dialog/dialog_helper.dart';
+import 'package:eliud_core/style/frontend/frontend_style.dart';
+import 'package:eliud_core/style/style_registry.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -10,27 +12,45 @@ import 'package:eliud_core/tools/storage/member_medium_helper.dart';
 typedef RichTextFeedback(String value);
 
 class RichTextDialog extends StatefulWidget {
+  static double width(BuildContext context) =>
+      MediaQuery.of(context).size.width * 0.9;
+
+  static double height(BuildContext context) =>
+      MediaQuery.of(context).size.height * 0.9;
+
   final String appId;
   final String title;
   final RichTextFeedback richTextFeedback;
   final String initialValue;
   final String ownerId;
   final List<String> readAccess;
+  final FrontEndStyle frontEndStyle;
 
   RichTextDialog({
     Key? key,
     required this.title,
     required this.richTextFeedback,
-    required this.initialValue, required this.appId, required this.ownerId, required this.readAccess,
+    required this.initialValue,
+    required this.appId,
+    required this.ownerId,
+    required this.readAccess,
+    required this.frontEndStyle,
   }) : super(key: key);
 
   @override
   _RichTextDialogState createState() => _RichTextDialogState();
 
-  static void open(BuildContext context, String appId, String ownerId, List<String> readAccess, String title,
-      RichTextFeedback richTextFeedback, String initialValue) {
-    DialogStatefulWidgetHelper.openIt(
-        context,
+  static void open(
+      BuildContext context,
+      String appId,
+      String ownerId,
+      List<String> readAccess,
+      String title,
+      RichTextFeedback richTextFeedback,
+      String initialValue,
+      FrontEndStyle frontEndStyle) {
+    frontEndStyle.dialogStyle().openWidgetDialog(context,
+        child:
         RichTextDialog(
           title: title,
           richTextFeedback: richTextFeedback,
@@ -38,88 +58,98 @@ class RichTextDialog extends StatefulWidget {
           appId: appId,
           ownerId: ownerId,
           readAccess: readAccess,
+          frontEndStyle: frontEndStyle,
         ));
   }
 }
 
 class _RichTextDialogState extends State<RichTextDialog> {
-  final DialogStateHelper dialogHelper = DialogStateHelper();
   final HtmlEditorController controller = HtmlEditorController();
   bool isHtml = false;
   double _progress = 1;
 
   bool _shouldUseNativeGrid(BuildContext context) {
-    double height = DialogStatefulWidgetHelper.height(context);
-    return  (height > 820);
+    double height = RichTextDialog.height(context);
+    return (height > 820);
   }
 
+  final DialogStateHelper dialogHelper = DialogStateHelper();
   @override
   Widget build(BuildContext context) {
-    return dialogHelper.build(context,
-        dialogButtonPosition: DialogButtonPosition.TopRight,
+    return widget.frontEndStyle.dialogWidgetStyle().flexibleDialog(
+        context,
         title: widget.title,
-        contents: GestureDetector(
-          onTap: () {
-            if (!kIsWeb) {
-              controller.clearFocus();
-            }
-          },
-          child: HtmlEditor(
-            controller: controller,
-            htmlEditorOptions: HtmlEditorOptions(
-              shouldEnsureVisible: true,
-              initialText: widget.initialValue,
-            ),
-            htmlToolbarOptions: HtmlToolbarOptions(
-                toolbarPosition: ToolbarPosition.aboveEditor,
-                toolbarType: _shouldUseNativeGrid(context) ? ToolbarType.nativeGrid : ToolbarType.nativeScrollable,
-                mediaUploadInterceptor: (platformFile, insertFileType) => _interceptUpload(platformFile, insertFileType)
-            ),
-            otherOptions: OtherOptions(
-                height: DialogStatefulWidgetHelper.height(context) - 130),
-          ),
-        ),
-//      seperator: LinearProgressIndicator( value:  _progress),
-        buttons: [
-          TextButton(
-              onPressed: isHtml ?  () {
-                isHtml = false;
-                controller.toggleCodeView();
-                setState(() {});
-              } : null,
-              child: Text('Visual')),
-          TextButton(
-              onPressed: !isHtml ? () {
-                isHtml = true;
-                controller.toggleCodeView();
-                setState(() {});
-              } : null,
-              child: Text('Html')),
-          Spacer(),
-          TextButton(
-            child: Text('Cancel'),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-          TextButton(
-            child: Text('Done'),
-            onPressed: () async {
-              Navigator.pop(context);
-              widget.richTextFeedback(await controller.getText());
-            },
-          ),
-        ]);
+        buttons: _buttons(),
+        child: _child());
   }
 
-  void _feedbackProgress(double progress) {
+  void _feedbackProgress(double progress) {}
+
+  List<Widget> _buttons() {
+    return [
+      widget.frontEndStyle.buttonStyle().dialogButton(context,
+          onPressed: isHtml
+              ? () {
+                  isHtml = false;
+                  controller.toggleCodeView();
+                  setState(() {});
+                }
+              : null,
+          label: 'Visual'),
+      widget.frontEndStyle.buttonStyle().dialogButton(
+            context,
+            label: 'Html',
+            onPressed: !isHtml
+                ? () {
+                    isHtml = true;
+                    controller.toggleCodeView();
+                    setState(() {});
+                  }
+                : null,
+          ),
+      Spacer(),
+      widget.frontEndStyle.buttonStyle().dialogButton(context, onPressed: () {
+        Navigator.pop(context);
+      }, label: 'Cancel'),
+      widget.frontEndStyle.buttonStyle().dialogButton(context,
+          onPressed: () async {
+        Navigator.pop(context);
+        widget.richTextFeedback(await controller.getText());
+      }, label: 'Done'),
+    ];
+  }
+
+  Widget _child() {
+    return /*GestureDetector(
+      onTap: () {
+        if (!kIsWeb) {
+          controller.clearFocus();
+        }
+      },
+      child: */
+        HtmlEditor(
+      controller: controller,
+      htmlEditorOptions: HtmlEditorOptions(
+        shouldEnsureVisible: true,
+        initialText: widget.initialValue,
+      ),
+      htmlToolbarOptions: HtmlToolbarOptions(
+          toolbarPosition: ToolbarPosition.aboveEditor,
+          toolbarType: _shouldUseNativeGrid(context)
+              ? ToolbarType.nativeGrid
+              : ToolbarType.nativeScrollable,
+          mediaUploadInterceptor: (platformFile, insertFileType) =>
+              _interceptUpload(platformFile, insertFileType)),
+      otherOptions: OtherOptions(height: RichTextDialog.height(context) - 130),
+    );
 /*
-    _progress = progress;
-    setState(() {});
+      ,
+    );
 */
   }
 
-  Future<bool> _interceptUpload(PlatformFile platformFile, InsertFileType insertFileType) async {
+  Future<bool> _interceptUpload(
+      PlatformFile platformFile, InsertFileType insertFileType) async {
     if (insertFileType == InsertFileType.audio) return false;
     var bytes = platformFile.bytes;
 
@@ -130,9 +160,18 @@ class _RichTextDialogState extends State<RichTextDialog> {
     if (insertFileType == InsertFileType.audio) return true;
 
     // All good
-    var baseName = platformFile.name!;
-    var thumbnailBaseName = BaseNameHelper.baseNameExt(baseName, 'thumbnail.png');
-    var memberMediumModel = await MemberMediumHelper.createThumbnailUploadPhotoData(widget.appId, bytes, baseName, thumbnailBaseName, widget.ownerId, widget.readAccess, feedbackProgress: _feedbackProgress);
+    var baseName = platformFile.name;
+    var thumbnailBaseName =
+        BaseNameHelper.baseNameExt(baseName, 'thumbnail.png');
+    var memberMediumModel =
+        await MemberMediumHelper.createThumbnailUploadPhotoData(
+            widget.appId,
+            bytes,
+            baseName,
+            thumbnailBaseName,
+            widget.ownerId,
+            widget.readAccess,
+            feedbackProgress: _feedbackProgress);
 
     String htmlCode;
     if (insertFileType == InsertFileType.video) {
