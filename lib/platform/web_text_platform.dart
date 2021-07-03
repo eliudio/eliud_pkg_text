1a187925223195c71b4991fcd45f02b020c597e2
Eliud <58745656+eliudio@users.noreply.github.com>
2021-07-03 18:55:40 +0100

    bugfix html editor web

--- lib/platform/web_text_platform.dart
+++ lib/platform/web_text_platform.dart
@@ -2,9 +2,11 @@ import 'package:eliud_pkg_text/platform/text_platform.dart';
 import 'package:eliud_pkg_text/platform/web/multiline_text_dialog.dart';
 import 'package:flutter/cupertino.dart';
 
+import 'mobile/html_text_dialog.dart';
+
 class WebTextPlatform extends AbstractTextPlatform {
   void updateHtml(BuildContext context, String appId, String ownerId, List<String> readAccess, String title,
       UpdatedHtml updatedHtml, String initialValue) {
-    MultilineTextDialog.open(context, appId, ownerId, readAccess, title, updatedHtml, initialValue);
+    HtmlTextDialog.open(context, appId, ownerId, readAccess, title, updatedHtml, initialValue);
   }
 }
