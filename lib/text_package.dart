ef1a9ce8273c30f9e25af03f4d540204c0a047a2
Eliud <58745656+eliudio@users.noreply.github.com>
2021-11-30 17:54:22 +0000

    Step 1 - Template - first draft - for assignment, works fine. Test assignment change: update assignment with assigneeId = CroHbMUvr7NDfWgaYKTP2Z5R8T92 and add -removed

--- lib/text_package.dart
+++ lib/text_package.dart
@@ -1,3 +1,4 @@
+import 'package:eliud_core/core/blocs/access/access_bloc.dart';
 import 'package:eliud_core/model/app_model.dart';
 import 'package:eliud_core/model/member_model.dart';
 import 'package:eliud_core/package/package.dart';
@@ -12,7 +13,7 @@ abstract class TextPackage extends Package {
   TextPackage() : super('eliud_pkg_text');
 
   @override
-  Future<bool?> isConditionOk(String packageCondition, AppModel app, MemberModel? member, bool isOwner, bool? isBlocked, PrivilegeLevel? privilegeLevel) async => null;
+  Future<bool?> isConditionOk(AccessBloc accessBloc, String pluginCondition, AppModel app, MemberModel? member, bool isOwner, bool? isBlocked, PrivilegeLevel? privilegeLevel) async => null;
 
   @override
   List<String>? retrieveAllPackageConditions() => null;
