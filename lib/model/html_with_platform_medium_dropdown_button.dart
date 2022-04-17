aaf1581a1df51c816dba7586c39968e7e418785b
Eliud <58745656+eliudio@users.noreply.github.com>
2022-04-17 22:29:44 +0100

    Dropdown button for components includes selector for privilege and allows to update the component

--- lib/model/html_with_platform_medium_dropdown_button.dart
+++ lib/model/html_with_platform_medium_dropdown_button.dart
@@ -24,22 +24,31 @@ import 'package:eliud_core/style/style_registry.dart';
 import 'package:eliud_core/core/blocs/access/state/access_state.dart';
 import 'package:eliud_core/core/blocs/access/access_bloc.dart';
 import 'package:eliud_core/style/frontend/has_text.dart';
+import 'package:eliud_core/tools/query/query_tools.dart';
+
+import 'package:eliud_core/style/frontend/has_button.dart';
+import 'package:eliud_core/tools/component/update_component.dart';
 
 
 import 'package:eliud_pkg_text/model/html_with_platform_medium_list_bloc.dart';
 import 'package:eliud_pkg_text/model/html_with_platform_medium_list_state.dart';
+import 'package:eliud_pkg_text/model/html_with_platform_medium_list_event.dart';
 import 'package:eliud_pkg_text/model/html_with_platform_medium_model.dart';
+import 'package:eliud_core/style/frontend/has_button.dart';
+import 'package:eliud_core/tools/component/update_component.dart';
 
 
-typedef HtmlWithPlatformMediumChanged(String? value);
+
+typedef HtmlWithPlatformMediumChanged(String? value, int? privilegeLevel,);
 
 class HtmlWithPlatformMediumDropdownButtonWidget extends StatefulWidget {
   final AppModel app;
-  final String? value;
+  int? privilegeLevel;
+  String? value;
   final HtmlWithPlatformMediumChanged? trigger;
   final bool? optional;
 
-  HtmlWithPlatformMediumDropdownButtonWidget({ required this.app, this.value, this.trigger, this.optional, Key? key }): super(key: key);
+  HtmlWithPlatformMediumDropdownButtonWidget({ required this.app, this.privilegeLevel, this.value, this.trigger, this.optional, Key? key }): super(key: key);
 
   @override
   State<StatefulWidget> createState() {
@@ -80,11 +89,13 @@ return widgets;
       if (state is HtmlWithPlatformMediumListLoading) {
         return StyleRegistry.registry().styleWithApp(widget.app).adminListStyle().progressIndicator(widget.app, context);
       } else if (state is HtmlWithPlatformMediumListLoaded) {
-        String? valueChosen;
-        if (state.values!.indexWhere((v) => (v!.documentID == widget.value)) >= 0)
-          valueChosen = widget.value;
-        else
-          if (widget.optional != null && widget.optional!) valueChosen = null;
+        int? privilegeChosen = widget.privilegeLevel;
+        if ((widget.value != null) && (privilegeChosen == null)) {
+          if (state.values != null) {
+            var selectedValue = state.values!.firstWhere((v) => (v!.documentID == widget.value), orElse: () => null);
+            privilegeChosen = selectedValue != null && selectedValue.conditions != null && selectedValue.conditions!.privilegeLevelRequired != null ? selectedValue.conditions!.privilegeLevelRequired!.index : 0;
+          }
+        }
           
         final values = state.values;
         final items = <DropdownMenuItem<String>>[];
@@ -114,28 +125,92 @@ return widgets;
                 )));
           });
         }
-        DropdownButton button = 
-                    DropdownButton<String>(
+        return ListView(
+            physics: ScrollPhysics(),
+            shrinkWrap: true,
+            children: [
+          DropdownButton<int>(
+            isDense: false,
+            isExpanded: false,
+            items: [
+              DropdownMenuItem<int>(
+                value: 0,
+                child: text(widget.app, context, 'No privilege Required'),
+              ),
+              DropdownMenuItem<int>(
+                value: 1,
+                child: text(widget.app, context, 'Level 1 privilege required'),
+              ),
+              DropdownMenuItem<int>(
+                value: 2,
+                child: text(widget.app, context, 'Level 2 privilege required'),
+              ),
+              DropdownMenuItem<int>(
+                value: 3,
+                child: text(widget.app, context, 'Must be owner'),
+              ),
+            ],
+            value: privilegeChosen,
+            hint: text(widget.app, context, 'Select a privilege'),
+            onChanged: _onPrivilegeLevelChange,
+          ),
+          Row(children: [(false)
+            ? Container(
+                height: 48, 
+                child: DropdownButton<String>(
                       isDense: false,
                       isExpanded: false,
                       items: items,
-                      value: valueChosen,
+                      value: widget.value,
                       hint: text(widget.app, context, 'Select a htmlWithPlatformMedium'),
-                      onChanged: !accessState.memberIsOwner(widget.app.documentID!) ? null : _onChange,
-                    );
-        if (false) {
-          return Container(height:48, child: button);
-        } else {
-          return button;
-        }
+                      onChanged: _onValueChange,
+                    )
+                ) 
+            : DropdownButton<String>(
+                isDense: false,
+                isExpanded: false,
+                items: items,
+                value: widget.value,
+                hint: text(widget.app, context, 'Select a htmlWithPlatformMedium'),
+                onChanged: _onValueChange,
+              ),
+          if (widget.value != null) Spacer(),
+          if (widget.value != null) 
+            Align(alignment: Alignment.topRight, child: button(
+              widget.app,
+              context,
+              icon: Icon(
+                Icons.edit,
+              ),
+              label: 'Update',
+              onPressed: () {
+                updateComponent(context, widget.app, 'htmlWithPlatformMediums', widget.value, (newValue) {
+                  setState(() {
+                    widget.value = widget.value;
+                  });
+                });
+              },
+            ))
+          ])
+        ]);
       } else {
         return StyleRegistry.registry().styleWithApp(widget.app).adminListStyle().progressIndicator(widget.app, context);
       }
     });
   }
 
-  void _onChange(String? value) {
-    widget.trigger!(value);
+  void _onValueChange(String? value) {
+    widget.trigger!(value, null);
+  }
+
+  void _onPrivilegeLevelChange(int? value) {
+    BlocProvider.of<HtmlWithPlatformMediumListBloc>(context).add(HtmlWithPlatformMediumChangeQuery(
+       newQuery: EliudQuery(theConditions: [
+         EliudQueryCondition('conditions.privilegeLevelRequired', isEqualTo: value ?? 0),
+         EliudQueryCondition('appId', isEqualTo: widget.app.documentID!),]
+       ),
+     ));
+     widget.trigger!(null, value);
   }
 }
 
