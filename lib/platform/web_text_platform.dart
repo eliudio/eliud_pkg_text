27a3147d82adb7b46bbaae0adf6782395794caa6
Eliud <58745656+eliudio@users.noreply.github.com>
2022-06-04 22:51:30 +0100

    Using local flutter_widget_from_html which is upgraded to flutter 3.1.0

--- lib/platform/web_text_platform.dart
+++ lib/platform/web_text_platform.dart
@@ -1,12 +1,16 @@
+import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
+/*
+import 'dart:html' as html;
+import 'dart:ui' as ui;
+import 'dart:js' as js;
+*/
+
 import 'package:eliud_core/model/app_model.dart';
 import 'package:eliud_core/model/member_medium_model.dart';
 import 'package:eliud_pkg_text/model/html_with_platform_medium_model.dart';
 import 'package:eliud_pkg_text/platform/text_platform.dart';
 import 'package:eliud_pkg_text/platform/widgets/handle_member_medium_model.dart';
-import 'package:eliud_pkg_text/platform/widgets/handle_platform_medium_model.dart';
-import 'package:eliud_pkg_text/platform/widgets/html_text_dialog.dart';
 import 'package:flutter/cupertino.dart';
-//import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 
 class WebTextPlatform extends AbstractTextPlatform {
   @override
@@ -53,6 +57,60 @@ class WebTextPlatform extends AbstractTextPlatform {
 
   @override
   Widget htmlWidget(String html) {
-    return Text("Nothing");
+    return HtmlWidget(html);
   }
 }
+
+/*
+class HtmlWidget extends StatefulWidget {
+  final String html;
+
+  const HtmlWidget({Key? key, required this.html}) : super(key: key);
+
+  @override
+  _HtmlState createState() => _HtmlState();
+}
+
+class _HtmlState extends State<HtmlWidget> {
+  late html.IFrameElement _element;
+
+  @override
+  void initState() {
+    _element = html.IFrameElement()
+*/
+/*
+      ..width = "200px"
+      ..height = "200px"
+*//*
+
+      ..style.border = 'none'
+      ..srcdoc = widget.html;
+
+*/
+/*
+    js.context["purchase_units"] = js.JsObject.jsify([
+      {
+        'amount': {'value': '0.02'}
+      }
+    ]);
+*//*
+
+
+    // ignore:undefined_prefixed_name
+    ui.platformViewRegistry.registerViewFactory(
+      'htmlPage',
+          (int viewId) => _element,
+    );
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 220,
+      height: 220,
+      child: HtmlElementView(viewType: 'htmlPage'),
+    );
+  }
+}*/
