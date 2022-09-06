da9572368f947ca04e5e9a014ff10163da213b0b
Eliud <58745656+eliudio@users.noreply.github.com>
2022-09-06 07:40:41 +0100

    Web bugfix

--- lib/platform/web_text_platform.dart
+++ lib/platform/web_text_platform.dart
@@ -1,24 +1,15 @@
-import 'package:eliud_core/core/registry.dart';
+import 'dart:convert';
+
 import 'package:eliud_core/model/app_model.dart';
-import 'package:eliud_core/model/member_medium_model.dart';
-import 'package:eliud_core/tools/action/action_model.dart';
-import 'package:eliud_core/tools/random.dart';
 import 'package:eliud_core/tools/screen_size.dart';
 import 'package:eliud_pkg_text/model/html_platform_medium_model.dart';
 import 'package:eliud_pkg_text/model/html_with_platform_medium_model.dart';
 import 'package:eliud_pkg_text/platform/text_platform.dart';
-import 'package:eliud_pkg_text/platform/widgets/handle_member_medium_model.dart';
 import 'package:eliud_pkg_text/platform/widgets/html_text_dialog.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:pointer_interceptor/pointer_interceptor.dart';
-import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
+import 'package:html_editor_enhanced/utils/utils.dart';
 import 'dart:html' as html;
 import 'dart:ui' as ui;
-import 'dart:js' as js;
-import 'package:eliud_core/core/navigate/router.dart' as router;
-import 'package:url_launcher/url_launcher.dart';
-
-import 'html_solution.dart';
 
 class WebTextPlatform extends AbstractTextPlatform {
   @override
@@ -67,7 +58,149 @@ class WebTextPlatform extends AbstractTextPlatform {
   }
 
   @override
-  Widget htmlWidgetWithPlatformMedia(BuildContext context, AppModel app, String html, {List<HtmlPlatformMediumModel>? htmlPlatformMedia}) {
-    return htmlSolution(context, app, html, htmlPlatformMedia: htmlPlatformMedia, webView: true);
+  Widget htmlWidgetWithPlatformMedia(
+      BuildContext context, AppModel app, String html,
+      {List<HtmlPlatformMediumModel>? htmlPlatformMedia}) {
+//    return htmlSolution(context, app, html, htmlPlatformMedia: htmlPlatformMedia, webView: true);
+    return MyHtmlWidget(html: html);
+  }
+}
+
+class MyHtmlWidget extends StatefulWidget {
+  final String html;
+
+  const MyHtmlWidget({Key? key, required this.html}) : super(key: key);
+
+  @override
+  _MyHtmlState createState() => _MyHtmlState();
+}
+
+class _MyHtmlState extends State<MyHtmlWidget> {
+  late String viewId;
+  late double actualHeight;
+  Future<bool>? initialised;
+
+  @override
+  void initState() {
+    actualHeight = 400;
+    viewId = getRandString(10);
+
+    super.initState();
+
+    final iframe = html.IFrameElement()
+      ..width = fullScreenWidth(context).toString() //'800'
+      ..height = fullScreenHeight(context).toString()
+      // ignore: unsafe_html, necessary to load HTML string
+      ..srcdoc = constructHtml(viewId, widget.html)
+      ..style.border = 'none'
+      ..style.overflow = 'hidden'
+      ..onLoad.listen((event) async {
+        var data = <String, Object>{'type': 'toIframe: getHeight'};
+        data['view'] = viewId;
+/*
+        var data2 = <String, Object>{'type': 'toIframe: setInputType'};
+        data2['view'] = viewId;
+*/
+        final jsonEncoder = JsonEncoder();
+        var jsonStr = jsonEncoder.convert(data);
+//        var jsonStr2 = jsonEncoder.convert(data2);
+        html.window.onMessage.listen((event) {
+          var data = json.decode(event.data);
+          if (data['type'] != null &&
+              data['type'].contains('toDart: htmlHeight') &&
+              data['view'] == viewId) {
+            final docHeight = data['height'] ?? actualHeight;
+            if ((docHeight != null && docHeight != actualHeight) &&
+                mounted &&
+                docHeight > 0) {
+              setState(mounted, this.setState, () {
+                actualHeight = docHeight;
+              });
+            }
+          }
+        });
+        html.window.postMessage(jsonStr, '*');
+/*
+        html.window.postMessage(jsonStr2, '*');
+*/
+      });
+    // ignore:undefined_prefixed_name
+    ui.platformViewRegistry.registerViewFactory(viewId, (int viewId) => iframe);
+    setState(mounted, this.setState, () {
+      initialised = Future.value(true);
+    });
+  }
+
+  String constructHtml(String createdViewId, String theHtml) => """
+<html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      <!--
+      <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+      <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
+      <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
+      -->
+      <style>
+      body {
+        overflow: hidden; /* Hide scrollbars */
+      }
+      </style>
+  </head>
+  <body>
+  <script type="text/javascript">
+    window.parent.addEventListener('message', handleMessage, false);
+    function handleMessage(e) {
+      if (e && e.data && e.data.includes("toIframe:")) {
+        var data = JSON.parse(e.data);
+        if (data["view"].includes("$createdViewId")) {
+          if (data["type"].includes("getHeight")) {
+            var height = document.body.scrollHeight;
+            window.parent.postMessage(JSON.stringify({"view": "$createdViewId", "type": "toDart: htmlHeight", "height": height}), "*");
+          }
+        }
+      }
+    }
+  </script>
+  <style>
+    body {
+        display: block;
+        margin: 0px;
+    }
+    .note-editor.note-airframe, .note-editor.note-frame {
+        border: 0px solid #a9a9a9;
+    }
+    .note-frame {
+        border-radius: 0px;
+    }
+  </style>
+  $theHtml
+  </body>
+</html>
+""";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: actualHeight,
+      child: Column(
+        children: <Widget>[
+          Expanded(
+              child: Directionality(
+                  textDirection: TextDirection.ltr,
+                  child: FutureBuilder<bool>(
+                      future: initialised,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          return HtmlElementView(
+                            viewType: viewId,
+                          );
+                        } else {
+                          return Container(height: actualHeight);
+                        }
+                      }))),
+        ],
+      ),
+    );
   }
 }
